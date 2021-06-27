2639eff04bc969ba01a773485d1ac3f853a6c514
David <david.stiven911@gmail.com>
2021-06-27 01:26:59 -0500

    some String constructors

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /*
+        String constructors
+         */
+
+        System.out.println("//Builds a string with the string passed as argument");
+        new String("this is a string ");
+
+        System.out.println("//Builds a string with the char array passed...");
+        new String (new char[]{'d', 'a', 'v', 'i', 'd'});
+
+        System.out.println("//Substring of the given char array" +
+                " from second argument index to third argument position");
+        String subName = new String(new char[]{'d', 'a', 'v', 'i', 'd'}, 0, 3);
+        System.out.println(subName);
+
+        
     }
 }
